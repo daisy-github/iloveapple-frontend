3e7bd2b6e22e3d5c3b2a58f5c101dc2ad52f2eda
daisy <daisy@spineor.com>
2018-11-18 14:53:04 +0530

    added code in app filr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import PostItem from "./Postitem";
@@ -6,8 +6,54 @@ import Sidebar from './Sidebar'
 import {
   Link,
 } from 'react-router-dom'
-import { Grid, Icon, Item} from 'semantic-ui-react'
+import { Grid, Icon, Item, Loader} from 'semantic-ui-react'
 import LayoutWrapper from './LayoutWrapper';
+class App extends Component {
+// fetchPostsByCategory = type => {
+//       <Query query={QUERY} variables={{type}} >
+//         {({ data, error, loading }) => {
+//              this.setState({data:data});
+//            }
+//           }
+//       </Query>
+//   }
+
+    render() {
+     return(
+      <Query query={QUERY} >
+        {({ data, error, loading }) => {
+          if (error) return '💩 Oops!';
+          if (loading) return <Grid.Row>
+              <Grid.Column computer={16} mobile={16} tablet={16} style={{padding:'40px 0'}}>
+                <Loader active inline='centered' />
+            </Grid.Column>
+          </Grid.Row>;
+
+          return (
+            <React.Fragment>
+             <Grid.Row>
+              <Grid.Column computer={10} mobile={16} tablet={10}>
+                <Item.Group divided className="wrapper">
+                  {data.GetPosts.map(post => (
+                    <PostItem post={post}/>
+                ))}
+                </Item.Group>
+              </Grid.Column>
+              <Grid.Column computer={6} mobile={16} tablet={6}>
+                <Sidebar fetchPostsByCategory={this.fetchPostsByCategory} />
+              </Grid.Column>
+            </Grid.Row>
+            </React.Fragment>
+              
+            
+
+          );
+        }}
+      </Query>
+    
+  )
+}
+}
 const QUERY = gql`
   query Posts{
     GetPosts{
@@ -28,41 +74,4 @@ const QUERY = gql`
   }
 `;
 
-const Posts = () => (
-      <Query query={QUERY} >
-        {({ data, error, loading }) => {
-          if (error) return '💩 Oops!';
-          if (loading) return 'Loading...';
-
-          return (
-            <React.Fragment>
-             <Grid.Row>
-              <Grid.Column computer={10} mobile={16} tablet={10}>
-                <Item.Group divided className="wrapper">
-                  {data.GetPosts.map(post => (
-                    <PostItem post={post}/>
-                ))}
-                </Item.Group>
-              </Grid.Column>
-              <Grid.Column computer={6} mobile={16} tablet={6}>
-                <Sidebar />
-              </Grid.Column>
-            </Grid.Row>
-            </React.Fragment>
-              
-            
-
-          );
-        }}
-      </Query>
-    
-
-      
-   
-  
-);
-
-
-const App = () => <Posts/>;
-
-export default LayoutWrapper(App)
+export default LayoutWrapper(App);
